fff4307bf7b81aa47249f9d1ef7cd11c6292eb70
Dinesh Bhattarai <dbhattarai252@gmail.com>
2019-02-07 00:41:12 +0545

    🐛 Optimizations

--- src/caesar.ts
+++ src/caesar.ts
@@ -1,17 +1,10 @@
 const letters = "abcdefghijklmnopqrstuvwxyz"
 
-export function encodeWord(word: string, key: number) {
-    return word.split("")
-        .map(c => letters.indexOf(c) != -1 ? letters[(letters.indexOf(c) + key) % letters.length] : c)
-//        .map(c => c[0].match(/[a-z]/) ? letters[(c.charCodeAt(0) + key - 97) % letters.length] : c[0])
-        .join("")
-
-}
 export function encode(msg: string, key: number) {
     return msg.toLowerCase()
-        .split(" ")
-        .map(word => encodeWord(word, key))
-        .join(" ")
+        .split("")
+        .map(c => letters.indexOf(c) != -1 ? letters[(letters.indexOf(c) + key) % letters.length] : c)
+        .join("")
 }
 
 export function decode(msg: string, key: number) {
--- test/caesar.spec.ts
+++ test/caesar.spec.ts
@@ -4,19 +4,20 @@ import { encode, decode } from "../src/caesar"
 let text = "hello world i am coming in 5000 seconds."
 
 describe("Caesar", function () {
-  it("Should return same on keys multiple of 26 and 0.", function () {
+  it("Should return identity when keys = multiple of 26 or 0.", function () {
     expect(encode(text, 0)).to.equal(text)
     expect(encode(text, 26)).to.equal(text)
     expect(encode(text, 52)).to.equal(text)
   })
-  it("Should return orginal value after encoding and decoding phase.", function () {
+  it("Should be reversible.", function () {
     expect(decode(encode(text, 22), 22)).to.equal(text)
     expect(decode(encode(text, 62), 62)).to.equal(text)
   });
 
-  it("Should not transform any character it does not understand including numbers", function(){
-    expect(encode("1234 ;./?", 2)).to.equal("1234 ;./?")
-    expect(decode("1234 ;./?", 2)).to.equal("1234 ;./?")
+  it("Should return identity fir any character it does not understand including numbers", function(){
+    let text = "1234 ;./?"
+    expect(encode(text, 2)).to.equal(text)
+    expect(decode(text, 2)).to.equal(text)
   })
 
   it("Should confirm to the output of other such programs.", function () {
